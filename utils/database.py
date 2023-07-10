bbdd64d0bb3122296dcb1f5a4fe68bec0e5042e1
Qing Wan <qing.wan@qantas.com.au>
2023-07-10 18:28:47 +1000

    init commit

---
+++ utils/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config import settings
+
+# default to use sqlite
+engine = create_engine(
+    settings.SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
+)
+
+# to use postgres db
+# engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, echo=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
